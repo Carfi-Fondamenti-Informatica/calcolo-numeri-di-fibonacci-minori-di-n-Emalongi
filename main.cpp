ef40ec0a4c01b1e53866df48f8745fed06a50aa1
Emalongi <91636607+Emalongi@users.noreply.github.com>
2021-11-10 23:30:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int a=1,b=1, n=0, i=0;
-    int c=0;
-    a=i;
-    cin>>n;
-    cout<<"1"<<endl;
-    for(int i=1;i<=n;i++){
-        c=a+b;
-         a=b;
-         b=c;
-        cout<< c<< endl;
-    }
-return 0;
+   int n=0,prec=0, succ=1,ris=0,i=0;
+   prec=i;
+   cin>>n;
+   cout<<"1"<<endl;
+   for(i=2;i<=n;i++){
+       ris=prec+succ;
+       prec=succ, succ=ris;
+       if(ris<=n){
+           cout<< ris<< endl;
+       }
+   }
+
+    return 0;
 }
