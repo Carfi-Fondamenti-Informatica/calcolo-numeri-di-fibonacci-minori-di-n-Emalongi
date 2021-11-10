902f93c478fa840fd60121b2ba25ac2763e82b23
Emalongi <91636607+Emalongi@users.noreply.github.com>
2021-11-10 21:28:20 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int a=1,b=1, n=0, i=0;
+    int c=0;
+    a=i;
+    cin>>n;
+    cout<<"1"<<endl;
+    for(i=1;i<=n;i++){
+        c=a+b;
+         a=b;
+         b=c;
+        cout<< c<< endl;
+    }
+return 0;
 }
